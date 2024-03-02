5db5cb535f50665fe9d572bee2a140e7ddbce942
fujiwara <mryh.flpc.mr0314@icloud.com>
2024-03-02 19:22:16 +0900

    first commit

---
+++ src/main/java/com/example/goaltrail/GoalTrailApplication.java
@@ -0,0 +1,13 @@
+package com.example.goaltrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalTrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalTrailApplication.class, args);
+	}
+
+}
