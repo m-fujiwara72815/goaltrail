fa2a61e16c74e38b621f2dfef6466261fb6b482a
fujiwara <mryh.flpc.mr0314@icloud.com>
2024-03-05 07:56:39 +0900

    Add:ログイン画成作成

---
+++ src/main/java/com/example/goaltrail/controller/PagesController.java
@@ -0,0 +1,14 @@
+package com.example.goaltrail.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PagesController {
+
+    @GetMapping("/")
+    public String index() {
+        return "users/login";
+    }
+}
+
