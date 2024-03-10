7fa71b9c0f214c1a998fc4f210cd5637c6ecf138
fujiwara <mryh.flpc.mr0314@icloud.com>
2024-03-10 14:09:44 +0900

    Add:サインイン画面作成 ・Entityの作成 ・PWのValidation機能追加 ・Formの作成 ・Repositoryとデータベースの設定 ・Securityの設定 ・ログの設定 ・Flashメッセージの設定 ・サインイン画面/登録完了画面作成 ・設定ファイル編集 ・リンクの編集 ・メソッドの変更 ・AWSデプロイ設定 ・メッセージプロパティ修正

--- src/main/java/com/example/goaltrail/GoalTrailApplication.java
+++ src/main/java/com/example/goaltrail/GoalTrailApplication.java
@@ -2,12 +2,18 @@ package com.example.goaltrail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GoalTrailApplication {
+public class GoalTrailApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoalTrailApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GoalTrailApplication.class);
+	}
 }
